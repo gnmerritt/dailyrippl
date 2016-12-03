f29a834e302f2b0740895846a92c3ebaef22c00e
Nathan Merritt <gnm@the-merritts.net>
2016-12-03 14:48:26 -0600

    Super basic user login form

--- rippl/rippl/urls.py
+++ rippl/rippl/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^login/$', auth_views.login, name='login'),
+    url(r'^logout/$', auth_views.logout, {'next_page': '/'}, name='logout'),
 ]
