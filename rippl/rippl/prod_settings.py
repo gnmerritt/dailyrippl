0c9608c25f10dac66aff015ff7fe649c182a7451
Nathan Merritt <gnm@the-merritts.net>
2016-12-05 20:44:25 -0500

    First cut at production settings

---
+++ rippl/rippl/prod_settings.py
@@ -0,0 +1,29 @@
+import os
+from .settings import *  # noqa F403
+
+DEBUG = False
+DEBUG_TEMPLATE = False
+
+SECRET_KEY = os.environ['SECRET_KEY']
+ALLOWED_HOSTS = [".gnmerritt.net", "*vendetta", "*scarab"]
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'rippl',
+        'USER': 'rippluser',
+        'PASSWORD': os.environ['RIPPL_PG_PASS'],
+        'HOST': 'localhost',
+        'PORT': '',
+    }
+}
+CONN_MAX_AGE = 30
+
+SLACK_API_KEY = os.environ['SLACK_API_KEY']
+
+CSRF_COOKIE_SECURE = True
+CSRF_COOKIE_HTTPONLY = True
+SESSION_COOKIE_SECURE = True
+SECURE_CONTENT_TYPE_NOSNIFF = True
+SECURE_BROWSER_XSS_FILTER = True
+X_FRAME_OPTIONS = 'DENY'
--- rippl/rippl/urls.py
+++ rippl/rippl/urls.py
@@ -1,18 +1,4 @@
-"""rippl URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/1.10/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.conf.urls import url, include
-    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
-"""
+"""rippl URL Configuration"""
 from django.conf.urls import include, url
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
@@ -22,7 +8,7 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^login/$', auth_views.login, name='login'),
     url(r'^logout/$', auth_views.logout, {'next_page': '/'}, name='logout'),
-    url(r'^/?$', TemplateView.as_view(template_name='index.html')),
+    url(r'^$', TemplateView.as_view(template_name='index.html')),
 
     url(r'^legislature/', include('legislature.urls')),
 ]
