c57ff63a00aac815148dbf0732fee8b96a2a5ff2
Nathan Merritt <gnm@the-merritts.net>
2016-12-06 18:56:43 -0500

    Add dependency install to update script

--- update_prod.sh
+++ update_prod.sh
@@ -3,13 +3,22 @@ set -e
 echo "Updating checkout"
 git pull
 
+echo "Installing deps"
+source /home/rippl/venvs/rippl/bin/activate # TODO: make this less snowflake
+pip install -r prod-requirements.txt
+# TODO: install npm so we can build frontend
+# npm install
+# brunch build
+
 echo "Updating static files"
+source PROD_ENV.sh
 export DJANGO_SETTINGS_MODULE=rippl.prod_settings
 python rippl/manage.py collectstatic --noinput
 rsync -avz static/ nathan@scorpion.local:~/rippl/static/
 
-if [ -f rippl-gunicorn.pid ]; then
+PIDFILE="rippl/gunicorn.pid"
+if [ -f "$PIDFILE" ]; then
   echo "Stopping running server"
-  kill `cat rippl-gunicorn.pid`
+  kill `cat "$PIDFILE"`
 fi
 bash start_prod.sh
