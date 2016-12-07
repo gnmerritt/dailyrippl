cbe9db45dfeb557cb91d87d87678a84c57a6155b
Nathan Merritt <gnm@the-merritts.net>
2016-12-06 22:30:27 -0500

    Add django-querycount

--- rippl/rippl/settings.py
+++ rippl/rippl/settings.py
@@ -32,6 +32,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'querycount.middleware.QueryCountMiddleware',
 ]
 
 ROOT_URLCONF = 'rippl.urls'
