83f23b0ee7ffd95648bcd265d9121298ebc5a54f
Nathan Merritt <gnm@the-merritts.net>
2016-12-06 22:35:35 -0500

    Add representatives table that doesn't totally suck

--- rippl/legislature/views.py
+++ rippl/legislature/views.py
@@ -3,7 +3,13 @@ from . import models
 
 
 class RepresentativeList(ListView):
-    model = models.Representative
+    context_object_name = 'reps'
+    template_name = "legislature/representative_list.html"
+
+    def get_queryset(self):
+        return models.ActiveTerm.objects \
+            .select_related() \
+            .order_by('-term__chamber')
 
 
 class RepresentativeDetail(DetailView):
