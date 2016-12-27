aa055d2e648938394569cddcf89f8bfa58ea1181
Nathan Merritt <gnm@the-merritts.net>
2016-12-26 20:48:02 -0500

    let javascript and django talk to each other even

---
+++ ui/js/profile/ProfileApp.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function ProfileApp() {
+  return (
+    <div>
+      Hello, profile app
+    </div>
+  );
+}
+
+ReactDOM.render(
+  <ProfileApp />,
+  document.getElementById('profile-app'),
+);
