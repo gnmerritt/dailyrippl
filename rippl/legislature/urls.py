7ea3608547864f8e508357f3ef849cfe4bb573d6
Carolyn Ranti <ranticarolyn@gmail.com>
2017-01-03 23:42:51 -0500

    make trailing slash optional

--- rippl/legislature/urls.py
+++ rippl/legislature/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     url(r'^reps/?$', v.RepresentativeList.as_view(), name='reps'),
     url(r'^reps/(?P<pk>\d+)/?$', v.RepresentativeDetail.as_view(), name='rep'),
     url(r'^reps/district/(?P<district_id>\d+)/?$', api.fetch_representative),
-    url(r'^reps/contact/', api.fetch_address_reps),
+    url(r'^reps/contact/?$', api.fetch_address_reps),
     url(r'^district/?$', api.find_district, name='find_district'),
 ]
