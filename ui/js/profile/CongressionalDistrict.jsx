61008c654aee0c2ab06a55066af1ae41bfcd88e1
Nathan Merritt <gnm@the-merritts.net>
2017-01-03 22:03:08 -0500

    Allow congressional district widget to fetch data on mount

--- ui/js/profile/CongressionalDistrict.jsx
+++ ui/js/profile/CongressionalDistrict.jsx
@@ -2,30 +2,40 @@ import React, { PropTypes } from 'react';
 import { connect } from 'react-redux';
 import { Button } from 'react-bootstrap';
 
-import { queryDistrict } from './ProfileActions';
+import { queryDistrict, fetchDistrict } from './ProfileActions';
 
 /**
  * Either show the user their congressional district or a button to set it
  */
-const CongressionalDistrict = (props) => {
-  const d = props.district;
-  if (!d.id) {
-    const loading = d.loading;
+class CongressionalDistrict extends React.Component {
+  componentDidMount() {
+    const d = this.props.district;
+    // fetch our district information using the saved district id
+    if (d.id && !d.state) {
+      this.props.fetchDistrict(d.id);
+    }
+  }
+
+  render() {
+    const d = this.props.district;
+    if (!d.state) {
+      const loading = d.loading;
+      return (
+        <Button
+          onClick={loading ? null : this.props.queryDistrict}
+          disabled={loading}
+        >
+          {loading ? 'Loading...' : 'Find my congressional district'}
+        </Button>
+      );
+    }
     return (
-      <Button
-        onClick={loading ? null : props.queryDistrict}
-        disabled={loading}
-      >
-        {loading ? 'Loading...' : 'Find my congressional district'}
-      </Button>
+      <div>
+        Your congressional district is {d.state.name} {d.number}
+      </div>
     );
   }
-  return (
-    <div>
-      Your congressional district is {d.state.name} {d.number}
-    </div>
-  );
-};
+}
 
 CongressionalDistrict.propTypes = {
   district: PropTypes.shape({
@@ -36,11 +46,13 @@ CongressionalDistrict.propTypes = {
     id: PropTypes.number,
     number: PropTypes.number,
   }).isRequired,
+  fetchDistrict: PropTypes.func.isRequired,
   queryDistrict: PropTypes.func.isRequired,
 };
 
 const stateToProps = state => ({ district: state.district });
 const dispatchToProps = dispatch => ({
+  fetchDistrict: id => dispatch(fetchDistrict(id)),
   queryDistrict: () => dispatch(queryDistrict()),
 });
 
