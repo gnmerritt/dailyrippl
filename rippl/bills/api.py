0b0cbbb8d97804fe14d08929d9f295cdf08e15b8
Carolyn Ranti <ranticarolyn@gmail.com>
2017-01-05 00:28:34 -0500

    add sponsor name to bills serializer

--- rippl/bills/api.py
+++ rippl/bills/api.py
@@ -3,9 +3,15 @@ from rest_framework import viewsets, serializers
 from bills.models import Bill
 
 
+class SponsorRelatedField(serializers.RelatedField):
+    def to_representation(self, value):
+        return '%s %s' % (value.first, value.last)
+
+
 class BillSerializer(serializers.ModelSerializer):
 
     topics = serializers.SlugRelatedField(many=True, read_only=True, slug_field='name')
+    sponsor = SponsorRelatedField(read_only=True)
 
     class Meta:
         model = Bill
